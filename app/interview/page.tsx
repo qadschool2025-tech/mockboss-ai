b4592a0b7513df179b2ffaf3b43211250cd2685c
qadschool2025-tech <qadschool2025@gmail.com>
2026-05-14 22:25:10 +0400

    Import loadConfig from getInterviewConfig

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useRef, useCallback } from 'react'
-import { useRouter } from 'next/navigation'
+import { loadConfig } from '../../lib/getInterviewConfig'
 
 interface VoiceAnalysis {
   wordsPerMinute: number

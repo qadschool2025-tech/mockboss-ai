d5c8aeae9ff21522d00f95de0e9f640bc7dee1d1
qadschool2025-tech <qadschool2025@gmail.com>
2026-06-09 09:33:27 +0400

    Update trigger.config.ts

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,1 +1,10 @@
 
+import { defineConfig } from '@trigger.dev/sdk'
+
+export default defineConfig({
+  project: 'proj_etxguqawnjcuivmivmdu',
+  runtime: 'node',
+  logLevel: 'log',
+  maxDuration: 60,
+  dirs: ['./trigger'],
+})

51a4cc49a421fb55b7fedc26e9a30017f3478aac
qadschool2025-tech <qadschool2025@gmail.com>
2026-06-14 12:41:29 +0400

    Update page.tsx

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -834,7 +834,7 @@ function ReportCover({
                 borderTop: '0.5px solid rgba(26,26,26,0.08)',
               }}
             >
-              "{assessment}"
+              {“${assessment}”}
             </div>
           )}
         </div>
